b874fe164fa1eda02335ac7d10dca6cc63306bd1
Todd McKenzie <toddmckenzie@hotmail.com>
2020-01-20 12:52:26 -0600

    created detail results html

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -6,4 +6,23 @@ from .models import Question, Choice
 # Get questions and display them
 
 def index(request):
-    return render(request, 'polls/index.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+#show specific question and choices
+
+def detail(request, question_id):
+    try:
+        question: Question.object.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question Does Not Exist")
+    
+    return render(request, 'polls/results.html', { 'question': question})
+
+#get question and display results
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})

ef6fd8418f90a76785b1284ec93b7bdd30e00e09
Todd McKenzie <toddmckenzie@hotmail.com>
2020-01-20 13:35:54 -0600

    voting working

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
 
 from .models import Question, Choice
 
@@ -15,14 +18,31 @@ def index(request):
 
 def detail(request, question_id):
     try:
-        question: Question.object.get(pk=question_id)
+        question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
-        raise Http404("Question Does Not Exist")
+        raise Http404('Question Does Not Exist')
     
-    return render(request, 'polls/results.html', { 'question': question})
+    return render(request, 'polls/detail.html', { 'question': question })
 
 #get question and display results
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice."
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    
